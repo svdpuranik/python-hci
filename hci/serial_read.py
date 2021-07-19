4920f9555d32704ea918f7ae5d705b18e78281e5
Sudhanshuvinayak Puranik <sudhanshuvinayak.puranik@diasemi.com>
2021-07-19 12:31:42 +0200

    - addded delays in serial_read

--- hci/serial_read.py
+++ hci/serial_read.py
@@ -1,11 +1,14 @@
 import serial
 import binascii
+import time
 
 def serial_read(ser):
+    time.sleep(0.3)
     data = bytearray()  # empty array for received bytes
     while True:  # execute this until bytes received on serial buffer
         bytes_to_read = ser.inWaiting()
         if bytes_to_read > 0:
+            time.sleep(0.3)
             break
     for i in range(bytes_to_read):
         data += bytearray(ser.read())  # fill the byte array with received bytes sequentially
